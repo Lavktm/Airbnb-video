3b809d2328778d74feda7259edcdad35be634309
lavktm <lav.ktm@gmail.com>
2023-11-07 11:49:47 +0530

    Element added for navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,5 +1,7 @@
 import Container from "../Container"
 import Logo from "../Logo"
+import Search from "./Search"
+import UserMenu from "./UserMenu"
 
 
 const Navbar = () => {
@@ -13,10 +15,13 @@ const Navbar = () => {
                     flex-row
                     items-center
                     justify-between
+                    gap-3 
                     md:gap-0
                     "
                     >
                         <Logo />
+                        <Search />
+                        <UserMenu />
                     </div>
                 </Container>
             </div>
