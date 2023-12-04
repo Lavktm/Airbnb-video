32071ca508a6d4c328bb0f6026615073f77ac2a6
lavktm <lav.ktm@gmail.com>
2023-12-04 14:02:22 +0530

    Prettier config added and registerModal

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -3,8 +3,9 @@
 import { BiSearch } from 'react-icons/bi'
 
 const Search = () => {
-    return (
-        <div className="
+  return (
+    <div
+      className="
         border-[1px]
         w-full
         md:w-auto
@@ -14,22 +15,27 @@ const Search = () => {
         hover:shadow-md
         transition
         cursor-pointer
-        ">
-            <div className="
+        "
+    >
+      <div
+        className="
           flex
           flex-row
           items-center
           justify-between 
-           ">
-                <div className="
+           "
+      >
+        <div
+          className="
             text-sm
             font-semibold
             px-6
-            ">
-                    Anywhere
-                </div>
-                <div
-                    className="
+            "
+        >
+          Anywhere
+        </div>
+        <div
+          className="
             hidden
             sm:block
             text-sm
@@ -39,12 +45,12 @@ const Search = () => {
             flex-1
             text-center
             "
-                >
-                    Any Week
+        >
+          Any Week
+        </div>
 
-                </div>
-
-                <div className="
+        <div
+          className="
                text-sm
                pl-6
                pr-2
@@ -53,27 +59,30 @@ const Search = () => {
                flex-row
                items-center
                gap-3
-               ">
-                    <div className="
+               "
+        >
+          <div
+            className="
                 hidden
                 sm:block
-                ">
-                        Add Guests
-                    </div>
-                    <div
-                        className="
+                "
+          >
+            Add Guests
+          </div>
+          <div
+            className="
                    p-2
                    bg-rose-500
                    rounded-full
                    text-white
-                    ">
-                        <BiSearch size={18} />
-
-                    </div>
-                </div>
-            </div>
+                    "
+          >
+            <BiSearch size={18} />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Search
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,26 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Nunito } from "next/font/google"
+import { Nunito } from 'next/font/google'
 
 import Navbar from './components/navbar/navbar'
 import ClientOnly from './components/ClientOnly'
-import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
 
 export const metadata: Metadata = {
   title: 'Airbnb',
-  description: 'Airbnb clone',
+  description: 'Airbnb clone'
 }
 
 const font = Nunito({
-  subsets: ["latin"]
+  subsets: ['latin']
 })
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal isOpen />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
